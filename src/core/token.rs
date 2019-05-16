3d2bc2cc7ffd732ea7bc12e3257df0ef806ced14
luoyou1014@gmail.com <zy921130>
2019-05-17 00:46:06 +0800

    完善注释

--- src/core/token.rs
+++ src/core/token.rs
@@ -4,8 +4,8 @@ pub struct Token{
 }
 
 impl Token{
-    const EOF:i8 = -1;
-    pub const EOL:&'static str = "\n";
+    const EOF:i8 = -1;  // 定义文件结束符
+    pub const EOL:&'static str = "\n";  // 定义换行符
 
     pub fn new(line_num: i32)->Self{
         Token{
