0caecf63df262aaf8859fcc1347b0ece81cecf1d
luoyou1014@gmail.com <zy921130>
2019-05-22 00:23:28 +0800

    添加读取字符功能

--- src/core/lexer.rs
+++ src/core/lexer.rs
@@ -13,7 +13,8 @@ pub struct Lexer {
 
     has_more: bool,      // 读入的文件是否还有未读的内容
 
-    queue: Queue<String>,
+    line_queue: Queue<String>,  // 行队列，将文件内容按行拆分形成队列
+    cur_line: Option<String>, // 当前行
 }
 
 impl Lexer{
@@ -22,16 +23,41 @@ impl Lexer{
             regex: regex,  // 正则字符串
             read: read,     // 读入的文件
             has_more: true,
-            queue: queue![]
+            line_queue: queue![],
+            cur_line: None
         };
     }
 
     pub fn read(mut self){
-        let fin = BufReader::new(self.read);
-        for line in fin.lines() {
-            self.queue.add(line.unwrap());
-        }
+        let mut f = BufReader::new(&self.read);
+        // for line in f.lines() {
+        //     self.line_queue.add(line.unwrap());
+        // }
+
         // let xxx = Token::new(10);
-        println!("查看队列结构：{:#?}", self.queue);
+        // println!("查看队列结构：{:#?}", self.line_queue);
+        // self.get_char();
+
+        let mut buf = Vec::<u8>::new();
+        while f.read_until(b'\n', &mut buf).expect("read_until 函数调用失败") != 0 {
+            // this moves the ownership of the read data to s
+            // there is no allocation
+            let s = String::from_utf8(buf).expect("from_utf8 函数调用失败");
+            println!("字符: {:#?}", s);
+            for c in s.chars() {
+                println!("字符: {}", c);
+            }
+            // this returns the ownership of the read data to buf
+            // there is no allocation
+            buf = s.into_bytes();
+            buf.clear();
+        }
     }
+
+    // fn get_char(mut self){
+    //     if(self.cur_line == None){
+    //         self.cur_line = Some(self.line_queue.remove().unwrap());
+    //     }
+    //     println!("查看队列结构：{:#?}", self.cur_line);
+    // }
 }
