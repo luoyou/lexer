e6c4323a48c8035fb52971f9e129c20fa3a5c0fa
luo <luoyou1014@163.com>
2019-05-22 13:02:57 +0800

    增加获取字符函数

--- src/core/lexer.rs
+++ src/core/lexer.rs
@@ -13,8 +13,8 @@ pub struct Lexer {
 
     has_more: bool,      // 读入的文件是否还有未读的内容
 
-    line_queue: Queue<String>,  // 行队列，将文件内容按行拆分形成队列
-    cur_line: Option<String>, // 当前行
+    cur_line: Vec<u8>, // 当前行
+    line_num: u32,     // 当前行号
 }
 
 impl Lexer{
@@ -23,41 +23,30 @@ impl Lexer{
             regex: regex,  // 正则字符串
             read: read,     // 读入的文件
             has_more: true,
-            line_queue: queue![],
-            cur_line: None
+            cur_line: Vec::<u8>::new()，
+            line_num: 0
         };
     }
 
     pub fn read(mut self){
+        self.get_char();
+    }
+
+    fn get_char(mut self){
         let mut f = BufReader::new(&self.read);
-        // for line in f.lines() {
-        //     self.line_queue.add(line.unwrap());
-        // }
 
-        // let xxx = Token::new(10);
-        // println!("查看队列结构：{:#?}", self.line_queue);
-        // self.get_char();
-
-        let mut buf = Vec::<u8>::new();
-        while f.read_until(b'\n', &mut buf).expect("read_until 函数调用失败") != 0 {
+        while f.read_until(b'\n', &mut self.cur_line).expect("read_until 函数调用失败") != 0 {
             // this moves the ownership of the read data to s
             // there is no allocation
-            let s = String::from_utf8(buf).expect("from_utf8 函数调用失败");
-            println!("字符: {:#?}", s);
+            let s = String::from_utf8(self.cur_line).expect("from_utf8 函数调用失败");
+            println!("行长度: {}", s.len());
             for c in s.chars() {
                 println!("字符: {}", c);
             }
             // this returns the ownership of the read data to buf
             // there is no allocation
-            buf = s.into_bytes();
-            buf.clear();
+            self.cur_line = s.into_bytes();
+            self.cur_line.clear();
         }
     }
-
-    // fn get_char(mut self){
-    //     if(self.cur_line == None){
-    //         self.cur_line = Some(self.line_queue.remove().unwrap());
-    //     }
-    //     println!("查看队列结构：{:#?}", self.cur_line);
-    // }
 }
