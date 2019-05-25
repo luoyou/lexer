74c482d9e75238be0621b38f1faba3e196dddbd1
luoyou1014@gmail.com <zy921130>
2019-05-26 01:58:43 +0800

    完善读取字符功能

--- src/core/lexer.rs
+++ src/core/lexer.rs
@@ -1,6 +1,3 @@
-extern crate queues;
-
-use queues::*;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
@@ -9,67 +6,87 @@ use super::token::Token;
 
 pub struct Lexer {
     regex: String,  // 正则字符串
-    read: File,     // 读入的文件
+    read: BufReader<File>,     // 读入的文件
 
     has_more: bool,      // 读入的文件是否还有未读的内容
 
-    cur_line: Vec<u8>, // 当前行
-    line_num: u32,     // 当前行号
+    cur_line: Vec<char>, // 当前行
+    line_num: u32,       // 当前行号
+    last_char: Option<char>      // 最近读取的字符
 }
 
 impl Lexer{
     pub fn new(regex: String, read: File)->Self{
         return Lexer{
             regex: regex,  // 正则字符串
-            read: read,     // 读入的文件
+            read: BufReader::new(read),     // 读入的文件
             has_more: true,
-            cur_line: Vec::<u8>::new(),
-            line_num: 0
+            cur_line: Vec::new(),
+            line_num: 0,
+            last_char: None
         };
     }
 
     pub fn read(mut self){
-        // self.get_char();
-        self.test()
+        
+        let mut word = String::from("");
+        let c:Option<char>;
+        loop {
+            c = self.get_char();
+            if(!self::is_space(c)){
+                break;
+            }
+        }
+
+        if(c == None){
+            return None;
+        }
+
+        if(isDigit(c)){
+            word.push(c.unwrap());
+            loop{
+                
+
+            }
+        }
+        return Some(word);
     }
 
-    // fn get_char(mut self)->Option<char>{
-    //     let mut f = BufReader::new(&self.read);
-    //     if !self.has_more {
-    //         return None;
-    //     } else {
-    //         let mut line = Vec::<u8>::new();
-    //         if f.read_until(b'\n', &mut line).expect("read_until 函数调用失败") != 0 {
-    //             // this moves the ownership of the read data to s
-    //             // there is no allocation
-    //             let mut xxx = String::from_utf8(line).expect("from_utf8 函数调用失败").chars();
-
-    //             let s = String::from_utf8(self.cur_line).expect("from_utf8 函数调用失败");
-    //             for c in s.chars() {
-    //                 println!("字符: {}", c);
-    //             }
-    //             return xxx.next();
-    //         }else{
-    //             return None;
-    //         }
-    //     }
-    // }
-
-    fn test(mut self){
-        let mut f = BufReader::new(&self.read);
-        let mut line = Vec::<u8>::new();
-        while f.read_until(b'\n', &mut line).expect("read_until 函数调用失败") != 0 {
-            // this moves the ownership of the read data to s
-            // there is no allocation
-            let s = String::from_utf8(line).expect("from_utf8 函数调用失败");
-            println!("行长度: {}", s.len());
-            for c in s.chars() {
-                self.cur_line.
-            }
-            // this returns the ownership of the read data to buf
-            // there is no allocation
-            line = s.into_bytes();
-            line.clear();
+    fn get_char(&mut self)->Option<char>{
+        if(self.last_char != None){
+            return self.last_char;
         }
+        if(self.cur_line.is_empty()){
+            let mut line = Vec::<u8>::new();
+            if(self.read.read_until(b'\n', &mut line).expect("read_until 函数调用失败") != 0) {
+                // this moves the ownership of the read data to s
+                // there is no allocation
+                let s = String::from_utf8(line).expect("from_utf8 函数调用失败");
+
+                for c in s.chars() {
+                    self.cur_line.push(c);
+                }
+                // println!("当前行：{:#?}", self.cur_line);
+                // this returns the ownership of the read data to buf
+                // there is no allocation
+                line = s.into_bytes();
+                line.clear();
+            }else{
+                return None;
+            }
+        }
+        return Some(self.cur_line.remove(0));
+    }
+
+    fn unget_char(&mut self, c: char){
+        self.last_char = Some(c);
+    }
+
+    fn is_space(c: Option<char>)->bool{
+        return c != None && c.unwrap().is_whitespace();
+    }
+
+    fn is_digit(c: Option<char>)->bool{
+        return c != None && c.unwrap().is_digit();
     }
 }
